5990d27470c6432261efd65fe535492db320a49f
Prince Yadav <princu2212@gmail.com>
2023-04-18 10:47:25 +0530

    Fix: update css in public file

--- resources/views/backend/main.blade.php
+++ resources/views/backend/main.blade.php
@@ -15,10 +15,6 @@
     <!-- DataTables -->
     <link href="{{ asset('backend/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css') }}" rel="stylesheet"
         type="text/css" />
-    <link href="{{ asset('backend/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css') }}" rel="stylesheet"
-        type="text/css" />
-    <link href="{{ asset('backend/libs/datatables.net-select-bs4/css//select.bootstrap4.min.css') }}" rel="stylesheet"
-        type="text/css" />
 
     <!-- Responsive datatable examples -->
     <link href="{{ asset('backend/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css') }}"
@@ -30,9 +26,6 @@
     <link href="{{ asset('backend/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
     <!-- App Css-->
     <link href="{{ asset('backend/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
-
-    <link rel="stylesheet" type="text/css"
-        href="https://cdn.jsdelivr.net/bootstrap.tagsinput/0.8.0/bootstrap-tagsinput.css">
 
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css">
 
@@ -78,14 +71,14 @@
     <script src="{{ asset('backend/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('backend/libs/metismenu/metisMenu.min.js') }}"></script>
     <script src="{{ asset('backend/libs/simplebar/simplebar.min.js') }}"></script>
-   
+    <!-- Required datatable js -->
+    <script src="{{ asset('backend/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('backend/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+
     <!-- Responsive examples -->
     <script src="{{ asset('backend/libs/datatables.net-responsive/js/dataTables.responsive.min.js') }}"></script>
     <script src="{{ asset('backend/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js') }}"></script>
 
-    <script src="{{ asset('backend/js/pages/dashboard.init.js') }}"></script>
-    <!-- Datatable init js -->
-    <script src="assets/js/pages/datatables.init.js"></script>
     <!-- App js -->
     <script src="{{ asset('backend/js/app.js') }}"></script>
 
@@ -113,20 +106,18 @@
             }
         @endif
     </script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
+    <!--tinymce js-->
+    <script src="{{ asset('backend/libs/tinymce/tinymce.min.js') }} "></script>
 
     <!-- init js -->
     <script src="{{ asset('backend/js/pages/form-editor.init.js') }} "></script>
 
-    <!-- Required datatable js -->
-    <script src="{{ asset('backend/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
-    <script src="{{ asset('backend/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
-
     <!-- Datatable init js -->
     <script src="{{ asset('backend/js/pages/datatables.init.js') }}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
     <script src="{{ asset('backend/js/code.js') }}"></script>
     <script src="{{ asset('backend/js/validate.min.js') }}"></script>
-    <script src="https://cdn.jsdelivr.net/bootstrap.tagsinput/0.8.0/bootstrap-tagsinput.min.js"></script>
 
 </body>
 
